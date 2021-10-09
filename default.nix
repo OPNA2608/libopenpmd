b580d0fd412eeb8831cca61f2eec3321b7065b4b
OPNA2608 <christoph.neidahl@gmail.com>
2021-10-09 21:35:25 +0200

    Include basic tests in nix script

--- default.nix
+++ default.nix
@@ -22,6 +22,20 @@ stdenv.mkDerivation rec {
 
   # stub until install step is implemented
   installPhase = ''
+    echo "##### LOADTEST #####"
+    ./tools/loadtest
+    echo "##### P86EXTRACT #####"
+    ./tools/p86extract test.P86
+    echo "##### P86CREATE #####"
+    ./tools/p86create TEST.***.RAW
+
+    echo "##### COMPARING HASHES #####"
+    sha256sum test.P86 TEST.P86
+    if [[ "$(sha256sum test.P86 | cut -d' ' -f1)" != "$(sha256sum TEST.P86 | cut -d' ' -f1)" ]]; then
+      echo "Hashes don't match!"
+      exit 1
+    fi
+
     touch $out
   '';
 }
