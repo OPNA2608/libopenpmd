75f8a38c742c2e4e78574190bc88770a40e42b38
OPNA2608 <christoph.neidahl@gmail.com>
2021-12-17 16:02:15 +0100

    Update shell.nix

--- shell.nix
+++ shell.nix
@@ -2,49 +2,18 @@ with import <nixpkgs> { };
 
 with pkgs;
 
-let
-  vimName = "vim-shell";
-  vimShell = vim_configurable.customize {
-    name = vimName;
-    vimrcConfig = {
-      customRC = ''
-        set nocompatible
-        set history=1024
-        if has ("syntax")
-          syntax on
-        endif
-        set ruler
-        set mouse-=a
-        set number
-        set laststatus=2
-        set noshowmode
-      '';
-      packages.myVimPackages = with vimPlugins; {
-        start = [
-          vim-nix
-          editorconfig-vim
-          vim-indent-guides
-          vim-better-whitespace
-          lightline-vim
-          vimsence
-        ];
-      };
-    };
-  };
-in
 mkShell {
   buildInputs = [
     cmake
     gcc
-    vimShell
     editorconfig-checker
     valgrind
     gdb
   ];
 
   shellHook = ''
-    export EDITOR="${vimShell}/bin/${vimName}"
-    echo "shell.nix-configured vim is available under: ${vimName}"
-    echo "EDITOR has been set to shell.nix-configured vim."
+    unset CC
+    unset CXX
+    export VERBOSE=1
   '';
 }
