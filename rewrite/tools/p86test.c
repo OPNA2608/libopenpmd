1cfa30557551e30e9101b0871ce13d7906d4eadf
Puna2608 <opna2608@protonmail.com>
2022-11-28 02:14:24 +0100

    p86test use cmdargs

--- rewrite/tools/p86test.c
+++ rewrite/tools/p86test.c
@@ -2,6 +2,19 @@
 
 #include <stdio.h>
 
+#define EXENAME "p86test"
+
+void help (void);
+
+void help (void) {
+	printf ("%s: Test functionality of P86 code by\n", EXENAME);
+	printf ("- loading a P86 file\n");
+	printf ("- printing details about its contents\n");
+	printf ("- exporting the parsed data into a new file with an identical file hash\n");
+	printf ("\n");
+	printf ("Usage:\t%s in.p86 out.p86\n", EXENAME);
+}
+
 /*
 	0 - OK
 	1 - main error
@@ -23,32 +36,43 @@
 	fclose (fileOut);\
 	CLEANUP_FILEIN;
 
-int main (void) {
+int main (int argc, char** argv) {
 	FILE* fileIn, * fileOut;
 	p86_struct* p86 = P86_New();
 
-	fileIn = fopen ("TEST.P86", "rb");
+	if (argc != 3) {
+		CLEANUP_P86;
+		help();
+		return RETVAL_MAIN;
+	}
+
+	fileIn = fopen (argv[1], "rb");
 	if (fileIn == NULL) {
+		printf ("%s failed: Opening %s with read access!\n", EXENAME, argv[1]);
 		CLEANUP_P86;
 		return RETVAL_MAIN;
 	}
 
 	if (P86_Read (p86, fileIn)) {
+		printf ("%s failed: Parsing %s!\n", EXENAME, argv[1]);
 		CLEANUP_FILEIN;
 		return RETVAL_LIB;
 	}
 
-	fileOut = fopen ("TEST_.P86", "wb");
+	fileOut = fopen (argv[2], "wb");
 	if (fileOut == NULL) {
+		printf ("%s failed: Opening %s with write access!\n", EXENAME, argv[2]);
 		CLEANUP_FILEIN;
 		return RETVAL_MAIN;
 	}
 
 	if (P86_Write (p86, fileOut)) {
+		printf ("%s failed: Writing parsed data to %s!\n", EXENAME, argv[2]);
 		CLEANUP_FILEOUT;
 		return RETVAL_LIB;
 	}
 
+	printf ("%s finished successfully!\n", EXENAME);
 	CLEANUP_FILEOUT;
 	return RETVAL_OK;
 }
